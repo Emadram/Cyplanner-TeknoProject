5e83671e46933083aa01171b382b054a428f3077
Emadram <refemad@outlook.com>
2025-03-28 18:02:02 +0200

    main.jsx lines: 1-1 11-10 13-11 15-12 19-14 | Type: Replacement | Reason: Update path

--- campus-book-swap/src/main.jsx
+++ campus-book-swap/src/main.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
@@ -8,17 +8,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Main Layout with shared header/footer */}
         <Route path="/" element={<MainLayout />}>
-          {/* Home page */}
           <Route index element={<Home />} />
-          
-          {/* Auth routes */}
-          <Route path="signup" element={<SignUp />} />
-          <Route path="signin" element={<SignIn />} />
-          
-          {/* 404 - Redirect to home */}
-          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
         </Route>
       </Routes>
     </BrowserRouter>
