12702f42e405d9a60f183b897662a16192841509
Emadram <refemad@outlook.com>
2025-03-28 16:05:13 +0200

    main.jsx lines: 1-1 5-7 7-28 | Type: Replacement | Reason: Setup of the routes

--- campus-book-swap/src/main.jsx
+++ campus-book-swap/src/main.jsx
@@ -1,11 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Main Layout with shared header/footer */}
+        <Route path="/" element={<MainLayout />}>
+          {/* Home page */}
+          <Route index element={<Home />} />
+          
+          {/* Auth routes */}
+          <Route path="signup" element={<SignUp />} />
+          <Route path="signin" element={<SignIn />} />
+          
+          {/* 404 - Redirect to home */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export default App;
