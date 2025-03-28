96427ebe89c1f1582eecb62dca3b346fe112f15b
Emadram <refemad@outlook.com>
2025-03-28 15:54:59 +0200

    Navbar.jsx lines: 5-5 9-9 | Type: Replacement | Reason: Updating Navbar

--- campus-book-swap/src/components/Navbar.jsx
+++ campus-book-swap/src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">
+    <nav className="flex justify-between items-center px-8 py-4 bg-gray-50 border-b border-gray-200">
+      <div className="text-xl font-bold">
         <Link to="/">YourApp</Link>
       </div>
-      <div className="navbar-menu">
-        <Link to="/" className="navbar-item">Home</Link>
-        <Link to="/signin" className="navbar-item">Sign In</Link>
-        <Link to="/signup" className="navbar-item">Sign Up</Link>
+      <div className="flex gap-4">
+        <Link to="/" className="text-gray-800 hover:text-gray-600">Home</Link>
+        <Link to="/signin" className="text-gray-800 hover:text-gray-600">Sign In</Link>
+        <Link to="/signup" className="text-gray-800 hover:text-gray-600">Sign Up</Link>
       </div>
     </nav>
   );
